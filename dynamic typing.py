c2edc6132bd61f1f978db55186a844b2298f770b
Svetlan-37 <karassvetlana@yandex.ru>
2024-07-14 14:07:35 +0300

    Add files via upload

---
+++ dynamic typing.py
@@ -0,0 +1,18 @@
+# вариант 1
+name = 'Svetlana'
+print(name)
+age = 55
+print(age)
+age_new = 56
+print(age_new)
+is_student = True
+print(is_student)
+# вариант 2
+name = 'Svetlana'
+print(name)
+age = 55
+print(age)
+age = (age + 1)
+print(age)
+is_student = True
+print(is_student)
